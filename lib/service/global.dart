4c529f462d9034e93a74a2836aa97bf424e33f56
EL FADILI MOHAMED <580.simo@gmail.com>
2024-04-30 02:46:59 +0100

    updating JSESSIONID setup

--- lib/service/global.dart
+++ lib/service/global.dart
@@ -2,4 +2,8 @@ import 'package:chatpotgemini/service/AuthService.dart';
 
 const String baseURL = "http://localhost:8080";
 const Map<String, String> headers = {"Content-Type": "application/json"};
-Map<String, String> ApiHeaders = {"Content-Type": "application/json",'Session-ID': AuthService().getSessionID().toString()};
+Map<String, String> ApiHeaders = {
+  "Content-Type": "application/json",
+  'Session-ID': AuthService().getSessionID().toString(),
+  'Cookie': 'JSESSIONID=${AuthService().getSessionID()}'
+};
